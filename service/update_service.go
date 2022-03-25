1d5d787f2fd511d40440d785d5c571fb53ca017a
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-03-25 14:00:39 +0900

    service: service

---
+++ service/update_service.go
@@ -0,0 +1,38 @@
+// Copyright 2022 The sacloud/phy-service-go Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import (
+	"context"
+
+	"github.com/sacloud/phy-api-go"
+	v1 "github.com/sacloud/phy-api-go/apis/v1"
+)
+
+func (s *Service) Update(req *UpdateRequest) (*v1.Service, error) {
+	return s.UpdateWithContext(context.Background(), req)
+}
+
+func (s *Service) UpdateWithContext(ctx context.Context, req *UpdateRequest) (*v1.Service, error) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	client := phy.NewServiceOp(s.client)
+	current, err := client.Read(ctx, v1.ServiceId(req.Id))
+	if err != nil {
+		return nil, err
+	}
+	return client.Update(ctx, v1.ServiceId(req.Id), req.ToRequestParameter(current))
+}
